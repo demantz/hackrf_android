64504ed7b4ed6b75b2e6bc3a6408152a11220ac9
Dennis Mantz <dennis.mantz@googlemail.com>
2014-10-02 21:47:06 -0700

    Switched to eclipse luna. No code modifications

--- gen/com/mantz_it/hackrf_android/R.java
+++ gen/com/mantz_it/hackrf_android/R.java
@@ -11,10 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static int ic_launcher=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static int app_name=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -38,13 +38,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f050001;
     }
     public static final class xml {
-        public static int device_filter=0x7f030000;
+        public static final int device_filter=0x7f030000;
     }
 }
--- src/com/mantz_it/hackrf_android/DeviceAttachedBroadcastReceiver.java
+++ src/com/mantz_it/hackrf_android/DeviceAttachedBroadcastReceiver.java
@@ -3,12 +3,17 @@ package com.mantz_it.hackrf_android;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.widget.Toast;
 
 public class DeviceAttachedBroadcastReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+		
+		
 		Toast.makeText(context, "HackRF One Attached!!!!.",Toast.LENGTH_LONG).show();
 	}
 
