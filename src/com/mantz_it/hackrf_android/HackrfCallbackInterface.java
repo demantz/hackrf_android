d566020b5ac125240695a3034e4f17ed9cfc7480
Dennis Mantz <dennis.mantz@googlemail.com>
2014-10-03 21:50:21 -0700

    Added Callback Interface

---
+++ src/com/mantz_it/hackrf_android/HackrfCallbackInterface.java
@@ -0,0 +1,45 @@
+package com.mantz_it.hackrf_android;
+
+/**
+ * <h1>HackRF USB Library for Android</h1>
+ * 
+ * Module:      HackrfCallbackInterface.java
+ * Description: This Interface declares a callback method to return a
+ * 				HackRF instance to the application after it was opened
+ * 				by the initialization routine (asynchronous process
+ * 				because it includes requesting the USB permissions)
+ * 
+ * @author Dennis Mantz
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+public interface HackrfCallbackInterface {
+
+	/**
+	 * Called by initHackrf() after the device is ready to be used.
+	 * 
+	 * @param hackrf	Instance of the HackRF that provides access to the device
+	 */
+	public void onHackrfReady(Hackrf hackrf);
+	
+	
+	/**
+	 * Called if there was an error when accessing the device.
+	 * 
+	 * @param message	Reason for the Error
+	 */
+	public void onHackrfError(String message);
+	
+}
