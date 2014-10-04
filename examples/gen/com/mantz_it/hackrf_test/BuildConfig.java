9c8d80b9184254594861eb1e1852c6cae9e8c4f4
Dennis Mantz <dennis.mantz@googlemail.com>
2014-10-03 22:11:40 -0700

    Cleaning up: Removing unused resources

---
+++ examples/gen/com/mantz_it/hackrf_test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mantz_it.hackrf_test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- examples/gen/com/mantz_it/hackrf_test/R.java
+++ examples/gen/com/mantz_it/hackrf_test/R.java
@@ -5,16 +5,41 @@
  * should not be modified by hand.
  */
 
-package com.mantz_it.hackrf_android;
+package com.mantz_it.hackrf_test;
 
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int action_settings=0x7f080002;
+        public static final int bt_openHackRF=0x7f080000;
+        public static final int tv_output=0x7f080001;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int main=0x7f070000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int action_settings=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int openHackRFOne=0x7f050003;
     }
     public static final class style {
         /** 
@@ -38,10 +63,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
